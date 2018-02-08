69a84d33a44faf8d8475a292ebd7328c9e17e54b
ebith <ebith.h@gmail.com>
2018-02-08 09:56:07 +0900

    Add error check

--- for-injection.js
+++ for-injection.js
@@ -11,9 +11,15 @@
   document.head.appendChild(script);
 
   fs.readdir(injectionDir, (err, files) => {
+    if (err) {
+      return console.log(err);
+    }
     for (const file of files) {
       if (/\.js$/.test(file)) {
         fs.readFile(path.join(injectionDir, file), 'utf-8', (err, userJs) => {
+          if (err) {
+            return console.log(err);
+          }
           const script = document.createElement('script');
           script.id = file.split('.')[0];
           script.innerHTML = '(()=>{' + userJs + '})();';
@@ -21,6 +27,9 @@
         });
       } else if (/\.css$/.test(file)) {
         fs.readFile(path.join(injectionDir, file), 'utf-8', (err, userCss) => {
+          if (err) {
+            return console.log(err);
+          }
           const style = document.createElement('style');
           style.id = file.split('.')[0];
           style.innerHTML = userCss;
